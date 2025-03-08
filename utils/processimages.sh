122e6987d25b1cf2381ff0e6b02fea617e263d0d
Andrew <dent.ace@gmail.com>
2025-03-08 17:20:29 +0000

    Add image processing script
    - Shell script to extract and process project images. Input files are perspective corrected, cropped screenshots.
    - Uses a `background_img` to subtract an averaged background for lighting correction.

---
+++ utils/processimages.sh
@@ -0,0 +1,178 @@
+#!/bin/bash
+
+# WARNING: Not safe for public use! Created for the author's benefit.
+# Assumes:
+# - Filenames and directory structure follows the project standard.
+# - Correct images are fed in
+# - ImageMagick, exiftool and Oxipng executables are required.
+
+
+# Check the required binaries are available
+for bin in 'magick' 'exiftool' 'oxipng'; do
+  if ! command -v "${bin}" &> /dev/null; then
+    echo "Error: '${bin}' is not installed or not in your PATH."
+    exit
+  fi
+done
+min_version="9.1.3" # Check Oxipng v9.1.3 or greater is available (for --zi)
+this_version=$(oxipng --version | head -n1 | awk '{print $NF}')
+if [[ $(printf '%s\n' "${min_version}" "${this_version}" \
+    | sort -V | head -n1) != "${min_version}" ]]; then
+  echo "Error: Oxipng v${this_version}; upgrade to at least v${min_version}."
+  exit
+fi
+# Check for at least one input file
+if [[ -z "$1" ]]; then
+  echo 'Missing filename. Provide at least one image to process.'
+  exit
+fi
+
+
+# Standard metadata for images
+readonly project='dex98.com'
+readonly copyright='This work is dedicated to the Public Domain by ACED, licensed under CC0.'
+readonly copyright_short='Public Domain by ACED, licensed under CC0.'
+readonly license='https://creativecommons.org/publicdomain/zero/1.0/'
+
+# Setup file paths
+base_dir=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
+art_dir=$(realpath "${base_dir}/../art")
+background_img="${base_dir}/background_img.png"
+
+echo ''
+echo 'Processing images...'
+
+
+while (( "$#" )); do
+
+  # Minimal checks for the input file
+  if [[ ! -f "${1%.*}.png" ]]; then
+    echo 'File not found. PNG file required.'
+    exit
+  fi
+  file_size=$(stat -f%z "$1")
+  if (( file_size < 102400 || file_size > 1048576 )); then
+    echo "File size is outside the allowed range (100 KiB - 1 MiB)."
+    exit
+  fi
+  if ! oxipng -q --pretend --opt 0 "$1"; then
+    echo "Not a valid PNG file. Check for image format issues."
+    exit
+  fi
+
+  # Get the image name and details from the filename
+  img_name="$( basename -s .png "$1" )" # e.g. `006-Dragon-1`
+  img_name="${img_name/_corrected/}" # Remove possible '_corrected' suffix
+  # Sanitize the string but accept edge cases, e.g. "Mr. Mime", "Farfetch'd"
+  img_name="$(echo "$img_name" | tr -c "a-zA-Z0-9'. -\n" '?')"
+  echo " > ${img_name}"
+
+  # Split the hyphenated string into separate elements
+  IFS='-' read -r dex_number dex_name dex_sprite <<< "${img_name}"
+  # Basic validation of the details
+  if ! [[ "$dex_number" =~ ^[0-9]{3}$ ]]; then
+    echo "Invalid 'dex number. Expected 3 digits with leading zeroes."
+    exit
+  fi
+  if [[ ! "$dex_name" =~ ^[A-Z] || ${#dex_name} -lt 3 ]]; then
+    echo "Invalid 'dex name. Expected TitleCase and at least 3 characters."
+    exit
+  fi
+  if [[ ! "$dex_sprite" =~ ^[0-2]$ ]]; then
+    echo "Invalid 'dex sprite number. Expected 0, 1, or 2."
+    exit
+  fi
+  img_title="${dex_number} ${dex_name} (${dex_sprite})" # e.g. `006 Dragon (1)`
+
+
+  # Produce images
+  # --------------
+
+  png_file="${art_dir}/png/${img_name}.png"
+  tmp_file="${art_dir}/png/${img_name}-temp.png"
+  magick "$1" -negate -morphology Dilate Disk:4 -morphology Erode Disk:2 \
+      -negate -sample 480x512 "${tmp_file}"
+  magick -colorspace gray -depth 8 "${tmp_file}" "${background_img}" \
+    -compose Mathematics -define compose:args=1,-0.8,0,0.5 -composite \
+    -auto-threshold OTSU -alpha off -sample 30x32 \
+    -define png:color-type=0 -define png:bit-depth=8 -define png:include-chunk=none \
+    "${png_file}"
+
+
+  # Create 'diff' images, for ultimate visual Quality Assurance
+  diff_file="${art_dir}/png/diff.${img_name}"
+  magick \
+    \( "${tmp_file}" -colorspace gray -depth 8 \) \
+    \( "${png_file}" -colorspace gray -depth 8 -sample 480x512 \) \
+    -alpha off -gravity center -compose difference -composite \
+    -normalize -contrast-stretch 5%x5% "${diff_file}".png
+  # Flicker animation for comparison
+  magick -delay 50 -loop 0 \
+    \( "${tmp_file}" -modulate 100,30,100 \) \
+    \( "${png_file}" +level-colors '#111','#888' \) \
+    -sample 480x512 -gravity center \
+    -dither FloydSteinberg -colors 256 -layers Optimize "${diff_file}".gif
+
+  # Create the gallery 'art' image
+  #  This is likely to be changed in the future, but useful placeholder
+  art_file="${art_dir%/*}/docs/gallery/${img_name}.png"
+  magick "${png_file}" -filter point -resize 330x320\! \
+    +level-colors '#001830','#BFBCB6' \
+    \( -size 11x10 canvas:none -fill none -stroke '#CFCDC7' -strokewidth 1 \
+      -draw "line 10,0 10,10" -draw "line 0,9 11,9" -write mpr:tile +delete \) \
+    \( -size 330x320 tile:mpr:tile \) \
+    -compose over -composite \
+    -sample 400% \
+    -negate -morphology Dilate Disk:1.5 -negate \
+    -filter Lanczos -resize 165x160 \
+    -dither None -colors 256 -define png:include-chunk=none \
+    "${art_file}"
+
+
+  # Optimize PNG image compression, before adding custom metadata
+  oxipng -q --nx --strip all "${png_file}" "${art_file}"
+  # First try to optimize with no reductions (8bpp, grayscale preferred)
+  #   then allow reductions (other bit depths and color formats)
+  for reductions in '-q --nx' '-q'; do
+    for zc_level in {0..12}; do
+      oxipng "${reductions}" --zc "${zc_level}" --filters 0-9 "${png_file}"
+    done
+    oxipng "${reductions}" --zopfli --zi 200 --filters 0-9 "${png_file}"
+  done
+
+  # Create the Portable Bit Map (PBM) file
+  pbm_file="${art_dir}/pbm/${img_name}.pbm"
+  magick "${png_file}" -depth 1 -compress None "${pbm_file}"
+
+
+  # Add metadata to png files first and then pbm file
+  exiftool \
+    "${png_file}" "${art_file}" -q -overwrite_original -fast1 \
+      -Title="#${img_title} - '${project}" \
+      -Copyright="${copyright_short} ${license}" \
+    -execute \
+    "${png_file}" -q -overwrite_original -fast1 \
+      -PNG:PixelsPerUnitX=909 -PNG:PixelsPerUnitY=1000 \
+    -execute \
+    "${pbm_file}" -q -overwrite_original -fast5 \
+      -Comment="${img_title} - '${project}" # Primary pbm metadata (single text line in header)
+  printf '\n# %s' "${copyright}" "${license}" >> "${pbm_file}" # Extra pbm metadata appended to the plain text file
+
+  # By adding image pixel resolution, we can account for non square-pixels,
+  #  alhtough most software only applies this when printing.
+  # exiftool -PNG:PixelsPerUnitX=909 -PNG:PixelsPerUnitY=1000 FILE.PNG
+  # TODO: Investigate overhead of including an EXIF chunk :
+  # exiftool -xresolution=23 -yresolution=25 -resolutionunit=inches FILE.PNG
+
+
+  # Remove temporary file
+  rm -f "${tmp_file}"
+
+  # Move to next image file provided
+  shift
+done
+
+
+echo '...Finished :)'
+echo ''
+exit
